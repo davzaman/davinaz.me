7866735c5181187c448d788ed6e3b64c50ddab97
DJ <davzaman@gmail.com>
2015-03-29 11:36:44 -0700

    change orientation to left, push sun right

--- interaction.js
+++ interaction.js
@@ -2,9 +2,9 @@ $(document).ready(function () {
 	$('.slide').each(function () {
 		var $bgobject = $(this);
 		$(window).scroll(function(){
-			var yPos = -($window.scrollTop() / %bjobject.data('speed'));
+			var yPos = -(window.scrollTop() / bjobject.data('speed'));
 			var coords = '50% ' + yPos + 'px';
-			%bgobject.css({background-position: coords});
+			bgobject.css({background-position: coords});
 		});
 	});
 });
