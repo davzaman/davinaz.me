f5cf9dad0c6b045de69e79313cd0f5eee2482083
DJ <davzaman@gmail.com>
2015-03-29 02:08:37 -0700

    changed bg photos, messing with gradient

---
+++ interaction.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+	$('.slide').each(function () {
+		var $bgobject = $(this);
+		$(window).scroll(function(){
+			var yPos = -($window.scrollTop() / %bjobject.data('speed'));
+			var coords = '50% ' + yPos + 'px';
+			%bgobject.css({background-position: coords});
+		});
+	});
+});
