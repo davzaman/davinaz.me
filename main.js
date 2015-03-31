e988f7a6648f7a219e9b142b0ec46148f271fa78
DJ <davzaman@gmail.com>
2015-03-30 18:14:12 -0700

    fix ul/li styling, include active nav as you scroll, include click nav to smooth scroll to slide

--- main.js
+++ main.js
@@ -3,10 +3,20 @@ var main = function() {
 	var degree = 0;
 	$(document).scroll(function() {
 		var nextScroll = $(this).scrollTop();
+		var currentSlide;
 		function rotate(degree){
 			$('#spokes').css({ WebkitTransform: 'rotate(' + degree + 'deg)'});
 			$('#spokes').css({ '-moz-transform': 'rotate(' + degree + 'deg)'});
 		}
+		$('.slide').each(function() {
+			var slidepos = $(this).offset().top;
+			if(slidepos - 80 < currentScroll){
+				$currentSlide = $(this);
+			}
+			var id = $currentSlide.attr('id');
+			$('a, li').removeClass('active-nav');
+			$("[href=#"+id+"] > li").addClass('active-nav');
+		});
 		if(nextScroll > currentScroll){
 			//downward scrolling
 			degree += (nextScroll - currentScroll);
@@ -18,6 +28,16 @@ var main = function() {
 		}
 		currentScroll = nextScroll;
 	});
+
+	$('a[href*=#]:not([href=#])').click(function() {
+     	 var target = $(this.hash);  
+     	 if (target.length) {
+        	$('html,body').animate({
+        	  scrollTop: target.offset().top
+        	}, 875);
+        	return false;
+      	}
+  	});
 };
 
 $(document).ready(main);
