b8e47ccd40eddae2e9454f2fc5f390d72ce2d3c3
davzaman <davzaman@gmail.com>
2018-08-23 13:44:08 -0700

    optimized images, fixed favicon, added credits and probably other stuff i forgot about

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 var main = function () {
+	// Expand content
 	$('.more').click(function () {
 		$(this).parent().find('.extra-content').toggle(400);
 	});
 
+	// Show credits
+	$('#credits').click(function () {
+		$('#more-credits').toggle(400);
+	});
+
+	// Timestamp per line on terminal
 	date = new Date(Date.now());
 	time =   date.getHours() + ":" + date.getMinutes();
 	$('.timestamp').append('[' + time + ']');
