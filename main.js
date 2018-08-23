106da798593a748b6064effb88608e573b9ce676
davzaman <davzaman@gmail.com>
2018-08-22 22:50:22 -0700

    added resume stuff, more face card stuff

--- main.js
+++ main.js
@@ -1,49 +1,17 @@
 var main = function () {
-	var currentScroll = 0;
-	var degree = 0;
-	$(document).scroll(function () {
-		var nextScroll = $(this).scrollTop();
-		var currentSlide;
-		function rotate(degree) {
-			$('#spokes').css({ WebkitTransform: 'rotate(' + degree + 'deg)' });
-			$('#spokes').css({ '-moz-transform': 'rotate(' + degree + 'deg)' });
-			$('#spokes').css({ '-o-transform': 'rotate(' + degree + 'deg)' });
-			$('#spokes').css({ 'transform': 'rotate(' + degree + 'deg)' });
-		}
-		$('.slide').each(function () {
-			var slidepos = $(this).offset().top;
-			if (slidepos - 110 < currentScroll) {
-				$currentSlide = $(this);
-			}
-			var id = $currentSlide.attr('id');
-			$('a, li').removeClass('active-nav');
-			$("[href=#" + id + "] > li").addClass('active-nav');
-		});
-		if (nextScroll > currentScroll) {
-			//downward scrolling
-			degree += (nextScroll - currentScroll);
-			rotate(degree);
-		}
-		else {
-			degree -= (currentScroll - nextScroll);
-			rotate(degree);
-		}
-		currentScroll = nextScroll;
-	});
-
-	$('a[href*=#]:not([href=#])').click(function () {
-		var target = $(this.hash);
-		if (target.length) {
-			$('html,body').animate({
-				scrollTop: target.offset().top
-			}, 875);
-			return false;
-		}
-	});
-
 	$('.more').click(function () {
 		$(this).parent().find('.extra-content').toggle(400);
 	});
+
+	// $('#resume').click(function(event) {
+	// 	event.preventDefault();
+	// 	window.location.href = 'files/DavinaZ_resume.pdf'
+	// });
+
+	// $('#cv').click(function(event) {
+	// 	event.preventDefault();
+	// 	window.location.href = 'files/DavinaZ_cv.pdf'
+	// });
 };
 
 $(document).ready(main);
