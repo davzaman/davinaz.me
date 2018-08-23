cc12dfeff2471341a6d872e92683528fd942559b
davzaman <davzaman@gmail.com>
2018-08-23 00:40:15 -0700

    ok last impulsive changes before i really give up for today.

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ var main = function () {
 		$(this).parent().find('.extra-content').toggle(400);
 	});
 
+	date = new Date(Date.now());
+	time =   date.getHours() + ":" + date.getMinutes();
+	$('.timestamp').append('[' + time + ']');
+
 	// $('#resume').click(function(event) {
 	// 	event.preventDefault();
 	// 	window.location.href = 'files/DavinaZ_resume.pdf'
