0576450280d3aeb65b9acbcc451a6daefbb0a3b9
davzaman <davzaman@gmail.com>
2018-08-22 18:14:18 -0700

    revamp in progress

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
-var main = function() {
+var main = function () {
 	var currentScroll = 0;
 	var degree = 0;
-	$(document).scroll(function() {
+	$(document).scroll(function () {
 		var nextScroll = $(this).scrollTop();
 		var currentSlide;
-		function rotate(degree){
-			$('#spokes').css({ WebkitTransform: 'rotate(' + degree + 'deg)'});
-			$('#spokes').css({ '-moz-transform': 'rotate(' + degree + 'deg)'});
-			$('#spokes').css({ '-o-transform': 'rotate(' + degree + 'deg)'});
-			$('#spokes').css({ 'transform': 'rotate(' + degree + 'deg)'});
+		function rotate(degree) {
+			$('#spokes').css({ WebkitTransform: 'rotate(' + degree + 'deg)' });
+			$('#spokes').css({ '-moz-transform': 'rotate(' + degree + 'deg)' });
+			$('#spokes').css({ '-o-transform': 'rotate(' + degree + 'deg)' });
+			$('#spokes').css({ 'transform': 'rotate(' + degree + 'deg)' });
 		}
-		$('.slide').each(function() {
+		$('.slide').each(function () {
 			var slidepos = $(this).offset().top;
-			if(slidepos - 110 < currentScroll){
+			if (slidepos - 110 < currentScroll) {
 				$currentSlide = $(this);
 			}
 			var id = $currentSlide.attr('id');
 			$('a, li').removeClass('active-nav');
-			$("[href=#"+id+"] > li").addClass('active-nav');
+			$("[href=#" + id + "] > li").addClass('active-nav');
 		});
-		if(nextScroll > currentScroll){
+		if (nextScroll > currentScroll) {
 			//downward scrolling
 			degree += (nextScroll - currentScroll);
 			rotate(degree);
@@ -31,15 +31,19 @@ var main = function() {
 		currentScroll = nextScroll;
 	});
 
-	$('a[href*=#]:not([href=#])').click(function() {
-     	 var target = $(this.hash);  
-     	 if (target.length) {
-        	$('html,body').animate({
-        	  scrollTop: target.offset().top
-        	}, 875);
-        	return false;
-      	}
-  	});
+	$('a[href*=#]:not([href=#])').click(function () {
+		var target = $(this.hash);
+		if (target.length) {
+			$('html,body').animate({
+				scrollTop: target.offset().top
+			}, 875);
+			return false;
+		}
+	});
+
+	$('.more').click(function () {
+		$(this).parent().find('.extra-content').toggle(400);
+	});
 };
 
 $(document).ready(main);
